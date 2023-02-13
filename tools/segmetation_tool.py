5d50fa422d8d311ba5853d959b30dfe6bf2fc547
Nikos Amvazas <nikosamvazas@gmail.com>
2023-02-13 19:24:50 +0200

    Update ...tools/segmentation_tool.py & Upload Raw EEG Data (EmotivEPOC_Flex)

--- tools/segmetation_tool.py
+++ tools/segmetation_tool.py
@@ -1,40 +1,74 @@
+import pandas as pd
 import matplotlib.pyplot as plt
-from matplotlib.patches import BoxStyle
-#import matplotlib.widgets as widgets
-#from matplotlib.widgets import TextBox
+from matplotlib.widgets import Button
+from tkinter import Tk, Text, TOP, BOTH, INSERT
 
-def on_right_click(event,fig,ax_list,signals,x_lst,points,windows,set_label_id) -> None:
+def show_description_window(event):
+    my_text = ("<Right Click> : Create fragment window" +
+                "\n<Backspace> : Delete last window" +
+                "\n<Enter> or <Esc> : Submit windows & Close\n\n")
+    root = Tk()
+    root.geometry("370x200")
+    root.title("Description")
+    text = Text(root, background="lightyellow")
+    text.tag_configure("black", foreground="black")
+    text.tag_configure("darkgrey", foreground="darkgrey")
+    text.tag_configure("red", foreground="red")
+    text.tag_configure("green", foreground="green")
+    text.tag_configure("blue", foreground="blue")
+    text.tag_configure("orange", foreground="orange")
+    text.insert(INSERT, my_text,"black")
+    text.insert(INSERT, "• <0> : Ιnactivity (default)\n", "darkgrey")
+    text.insert(INSERT, "• <1> : Left Movement\n", "red")
+    text.insert(INSERT, "• <2> : Right Movement\n", "green")
+    text.insert(INSERT, "• <3> : Up Movement\n", "blue")
+    text.insert(INSERT, "• <4> : Down Movement\n", "orange")
+    text.config(state="disabled")
+    text.pack(side=TOP, fill=BOTH, expand=True)
+    root.mainloop()
+
+def on_right_click(event,fig,ax_list,signals,movements_list,points,windows,label_id,sample_rate,window_lenght) -> None:
+    
     """
     Function to handle right click events.    
     """
-    sample_rate = 256
-    
-    if set_label_id[0] == 0 : color = "black"
-    elif set_label_id[0] == 1 : color = "red"
-    elif set_label_id[0] == 2 : color = "green"
-    elif set_label_id[0] == 3 : color = "blue"
+    # Set label ID
+    if label_id[0] == 0 : color = "darkgrey"
+    elif label_id[0] == 1 : color = "red"
+    elif label_id[0] == 2 : color = "green"
+    elif label_id[0] == 3 : color = "blue"
+    elif label_id[0] == 4 : color = "orange"
     # Check if the event was a right click
     if event.button == 3:
         # Get the x-axis position of the right click
         x = int(event.xdata)
-        # Add a x
-        x_lst.append(x)
-        # Add a colored dot to the plot
-        for i in range(len(ax_list)):
-            points[i].append(ax_list[i].scatter(x, signals[i][x], c=color, zorder=2))
-
-        # Add a vertical span to the plot
-        span_start = max(x - 2*sample_rate, 0)
-        span_end = min(x + 2*sample_rate, len(signals[0]))
-        for i in range(len(ax_list)):
-            windows[i].append(ax_list[i].axvspan(span_start, span_end, color='yellow', alpha=0.4))
-
-        # Update the plot
-        fig.canvas.draw()
+        # Create window borders
+        span_start = x - (window_lenght/2)*sample_rate
+        span_end = x + (window_lenght/2)*sample_rate
+        # Check if window borders are correct
+        if(span_start>=0 and span_end<=len(signals[0])):
+            # Add movement
+            movements_list[0].append(label_id[0]), movements_list[1].append(x)
+            # Add a colored dot to each signal plot
+            for i in range(len(ax_list)):
+                points[i].append(ax_list[i].scatter(x, signals[i][x], c=color, zorder=2))
+            # Add a vertical span to each signal plot
+            for i in range(len(ax_list)):
+                windows[i].append(ax_list[i].axvspan(span_start, span_end, color=color, alpha=0.4))
+            # Print message to console
+            print("Segmentation Added")
+            # Update plots
+            fig.canvas.draw()
+        else: print("WARNING ---> The window you define is out of the signal borders. Please define a new window.")
         
-def on_key_press(event,fig,x_lst,points,windows,set_label_id):
+def on_key_press(event,fig,movements_list,points,windows,label_id) -> None:
+    
     """
-    Connect the key press event to the function.    
+    Connect the key press event to the function.
+    Keyboard buttons description:
+        - Backspace: Erase last segmentation
+        - ESC or Enter: Close plot / Submit your signal windows
+        - Numbers 0-4: Set label    
     """
     if event.key == 'backspace':
         if len(points[0])>0:
@@ -43,90 +77,148 @@ def on_key_press(event,fig,x_lst,points,windows,set_label_id):
                 point.remove()
                 window = windows[i].pop()
                 window.remove()
-            x_lst.pop()
+            movements_list[0].pop(), movements_list[1].pop()
+            print("Last Segmentation Erased")
         else:
             print("Empty")
         fig.canvas.draw()
     elif event.key == 'escape' or event.key == 'enter':
         plt.close()
     elif event.key == '0':
-        set_label_id.pop()
-        set_label_id.append(0)
+        label_id.pop()
+        label_id.append(0)
     elif event.key == '1':
-        set_label_id.pop()
-        set_label_id.append(1)
+        label_id.pop()
+        label_id.append(1)
     elif event.key == '2':
-        set_label_id.pop()
-        set_label_id.append(2)
+        label_id.pop()
+        label_id.append(2)
     elif event.key == '3':
-        set_label_id.pop()
-        set_label_id.append(3)
-        
-        
-def signal_segmentation(signals,signal_names = ["EEG.F7","EEG.F8","TP9","TP10"]):
-
-    # Create a figure and two axes
-    fig, (ax1, ax2, ax3, ax4 ) = plt.subplots(4, 1)
-    fig.subplots_adjust(left=0.25), fig.subplots_adjust(right=0.975)
-    fig.subplots_adjust(top=0.95), fig.subplots_adjust(bottom=0.09)
-
- 
-    # add text description in a box
-    title = "Muse 2 WM"
-    description = ("Description: " +
-                "\n<Right Click> : Create fragment window" +
-                "\n<Backspace> : Delete last window"+
-                "\n<Enter> or <Esc> : Submit windows & Close"+
-                "\n<0> : " +
-                "\n<1> : " +
-                "\n<2> : " +
-                "\n<3> : ") 
-    plt.figtext(0.07, 0.65, title, bbox=dict(facecolor='green', alpha=0.5, boxstyle=BoxStyle("round", pad=0.5)), fontsize=17,va='center', ha='left')
-    plt.figtext(0.02, 0.5, description, bbox=dict(facecolor='lightblue', alpha=0.5, boxstyle=BoxStyle("round", pad=0.5)), fontsize=10,va='center', ha='left')
+        label_id.pop()
+        label_id.append(3)
+    elif event.key == '4':
+        label_id.pop()
+        label_id.append(4)
+         
+def create_figure(signals, muse_type, Fs, window_lenght):
+    print(len(signals))
+    if(muse_type == "EEG" and len(signals) == 4):
+        signal_names = ["F7","F8","TP9","TP10"]
+        # Create a figure and axes
+        fig, (ax1, ax2, ax3, ax4) = plt.subplots(4, 1)
+        #fig.subplots_adjust(left=0.25), fig.subplots_adjust(right=0.975)
+        fig.subplots_adjust(top=0.93), fig.subplots_adjust(bottom=0.09)
+        fig.text(0.5, 0.04, 'Samples', ha='center')
+        fig.text(0.05, 0.5, 'Amplitube (Voltage [$\mu V$])', va='center', rotation='vertical')
+        fig.suptitle('EEG Signals Segmentation/Labeling', fontsize=16)
     
-    # Plot signals
-    ax1.plot(signals[0],label=signal_names[0])
-    ax1.legend()
-    ax2.plot(signals[1])
-    ax3.plot(signals[2])
-    ax4.plot(signals[3])
-    #Set Grids
-    ax1.grid(), ax2.grid(), ax3.grid(), ax4.grid()
+        # Plot signals
+        ax1.plot(signals[0],label=signal_names[0],color="blue")
+        ax2.plot(signals[1],label=signal_names[1],color="darkcyan")
+        ax3.plot(signals[2],label=signal_names[2],color="red")
+        ax4.plot(signals[3],label=signal_names[3],color="orange")
+        ax1.legend(loc='upper left'),ax2.legend(loc='upper left'),ax3.legend(loc='upper left'),ax4.legend(loc='upper left')
+        # Set Grids
+        ax1.grid(), ax2.grid(), ax3.grid(), ax4.grid()
+        axs = [ax1,ax2,ax3,ax4]
 
-    # Keep track of the points and windows for 4 signals
-    ax1_points, ax2_points, ax3_points, ax4_points = [], [], [], []
-    ax1_windows, ax2_windows, ax3_windows, ax4_windows = [], [], [], []
-    points = [ax1_points, ax2_points, ax3_points, ax4_points]
-    windows = [ax1_windows, ax2_windows, ax3_windows, ax4_windows]
-    x_lst = []
-    set_label_id = [0]
-     
+        # Keep track of the points and windows for 4 signals
+        # Init points and windows lists
+        ax1_points, ax2_points, ax3_points, ax4_points = [], [], [], []
+        ax1_windows, ax2_windows, ax3_windows, ax4_windows = [], [], [], []
+        points = [ax1_points, ax2_points, ax3_points, ax4_points]
+        windows = [ax1_windows, ax2_windows, ax3_windows, ax4_windows]
+    elif( (muse_type == "Gyroscope" or muse_type == "gyroscope") and len(signals) == 3):
+        signal_names = ["X","Y","Z"]
+        # Create a figure and axes
+        fig, (ax1, ax2, ax3) = plt.subplots(3, 1)
+        fig.subplots_adjust(top=0.93), fig.subplots_adjust(bottom=0.09)
+        fig.text(0.5, 0.04, 'Samples', ha='center')
+        fig.text(0.05, 0.5, 'Amplitube ', va='center', rotation='vertical')
+        fig.suptitle('Gyroscope Signals Segmentation/Labeling', fontsize=16)
+        # Plot signals
+        ax1.plot(signals[0],label=signal_names[0],color="red")
+        ax2.plot(signals[1],label=signal_names[1],color="green")
+        ax3.plot(signals[2],label=signal_names[2],color="blue")
+        ax1.legend(loc='upper left'),ax2.legend(loc='upper left'),ax3.legend(loc='upper left')
+        # Set Grids
+        ax1.grid(), ax2.grid(), ax3.grid()
+        axs = [ax1,ax2,ax3]
+        ax1_points, ax2_points, ax3_points = [], [], []
+        ax1_windows, ax2_windows, ax3_windows = [], [], []
+        points = [ax1_points, ax2_points, ax3_points]
+        windows = [ax1_windows, ax2_windows, ax3_windows]
+    else:
+        print("ERROR")
+        return [-1],[-1]
+    
+    # Init movements list
+    movements_list = [[],[]]
+    # Init label ID
+    label_id = [0]
+
+    # Create Description Button
+    plt.subplots_adjust(bottom=0.2)
+    callback = show_description_window
+    ax_button = plt.axes([0.8, 0.05, 0.1, 0.075])
+    button = Button(ax_button, 'Description')
+    button.on_clicked(callback)
+
     # Connect the keyboard events to the function
-    fig.canvas.mpl_connect('key_press_event', lambda event: on_key_press(event,fig,x_lst,points,windows,set_label_id))
+    fig.canvas.mpl_connect('key_press_event', lambda event: on_key_press(event,fig,movements_list,points,windows,label_id))
     # Connect the right click event to the function
-    fig.canvas.mpl_connect("button_press_event", lambda event: on_right_click(event,fig,[ax1,ax2,ax3,ax4],signals,x_lst,points,windows,set_label_id ))
+    fig.canvas.mpl_connect("button_press_event", lambda event: on_right_click(event,fig,axs,signals,movements_list,points,windows,label_id, Fs, window_lenght))
 
-    # Show the plot
+    # Maximize fig window
     figManager = plt.get_current_fig_manager()
     figManager.window.showMaximized()
+    # Show the plot
     plt.show()
-    return x_lst
+    # Return movements list
+    return movements_list[0], movements_list[1] 
 
 
+def signal_Segmentation(dataframe:pd, Fs=128, window_lenght = 4):
+
+    """
+    Documant MUSE_2 Signal Segmentation
+    """
+    signals = dataframe.transpose().values
+    if (len(signals)==4): muse_mode = "EEG"
+    elif (len(signals)==3) : muse_mode = "Gyroscope"
+    else:
+        empty_df = pd.DataFrame() 
+        print("WARNING ---> The dataframe structure being imported is incorrect.\n" +
+                "The dataframe must have:\n" +
+                "Exactly 4 signals (F7,F8,TP9,TP10) in EEG mode or\n Exactly 3 signals (X,Y,Z) in Gyroscope mode.")
+        return empty_df
+
+
+    labels_list, window_markers_list = create_figure(signals, muse_mode, Fs, window_lenght)
+    if (labels_list and window_markers_list):
+        # Combine the two lists into a list of tuples
+        zipped_lists = list(zip(labels_list, window_markers_list))
+        # Sort the list of tuples based on the first element of each tuple
+        zipped_lists.sort(key=lambda x: x[0])
+        # Unpack the sorted list of tuples back into two separate lists
+        labels_list, window_markers_list = zip(*zipped_lists)
+
+    print(labels_list)
+    print(window_markers_list) 
+
+
+
+"""""
+#Main only for testing purposes
 def main():
-    print("Hello World!")
-        # Create a sample signal
-    samples = 128
-    signals = []
-    signal1 = [i for i in range(samples*100)]
-    signals.append(signal1)
-    signal2 = [i**2 for i in range(samples*100)]
-    signals.append(signal2)
-    signals.append(signal1)
-    signals.append(signal1)
-    
-      # Create a sample signal
-    print(signal_segmentation(signals))
+    try:
+        dataframe = pd.read_csv("trial1_15.12.19_21.19.34.csv",
+                                header = 1,usecols = ['EEG.F7','EEG.F8','EEG.TP9']) 
+        dataframe = dataframe.loc[:, ['EEG.F7','EEG.F8','EEG.TP9']]
+    except ValueError:
+            print("Oops! There was a problem with file: ")
+    signal_Segmentation(dataframe)
 
 if __name__ == "__main__":
     main()
+"""""
